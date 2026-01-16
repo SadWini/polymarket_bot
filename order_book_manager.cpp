55738763bdc27108673769a6560724a2da3e5b2f
SadWini <skylorun@gmail.com>
2026-01-16 19:32:08 +0300

    Created files

---
+++ order_book_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sky on 14.01.2026.
+//
+
+#include "order_book_manager.h"
---
+++ order_book_manager.h
@@ -0,0 +1,16 @@
+//
+// Created by Sky on 14.01.2026.
+//
+
+#ifndef ORDER_BOOK_MANAGER_H
+#define ORDER_BOOK_MANAGER_H
+
+
+
+class order_book_manager {
+
+};
+
+
+
+#endif //ORDER_BOOK_MANAGER_H
